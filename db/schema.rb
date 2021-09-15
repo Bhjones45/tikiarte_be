fa3d3afb1eedcaa74eaa1d4786744444d1bd1655
Brett Jones <bhjones4545@gmail.com>
2021-09-15 15:42:59 -0600

    db setup

---
+++ db/schema.rb
@@ -0,0 +1,46 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_09_15_211616) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.bigint "director_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["director_id"], name: "index_artists_on_director_id"
+  end
+
+  create_table "directors", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "status", default: "private"
+    t.string "url"
+    t.bigint "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_images_on_artist_id"
+  end
+
+  add_foreign_key "artists", "directors"
+  add_foreign_key "images", "artists"
+end
