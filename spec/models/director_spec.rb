a2bd5226225c93e3b0b96bf6a650d2bf4d117026
Brett Jones <bhjones4545@gmail.com>
2021-09-15 15:43:33 -0600

    director associations and validations

---
+++ spec/models/director_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Director, type: :model do
+  describe 'associations' do
+    it { should have_many :artists }
+    it { should have_many(:images).through(:artists)}
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:token) }
+    it { should validate_uniqueness_of(:token) }
+  end
+end
