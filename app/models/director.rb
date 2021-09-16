372c0cc532216673c1c81288ac3fd418cf570fa4
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-16 14:04:05 -0600

    Remove unneeded validations

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,5 +2,4 @@ class Director < ApplicationRecord
   has_many :artists
   has_many :images, through: :artists
   validates :email, uniqueness: true, presence: true
-  validates :token, uniqueness: true, presence: true
 end
