ce3056d70f601c68f2a806db1afaabb3c206f761
danembb <dbrophy720@gmail.com>
2021-09-17 17:27:14 -0600

    Provide additional testing for returned_director

--- spec/requests/api/v1/directors/directors_request_spec.rb
+++ spec/requests/api/v1/directors/directors_request_spec.rb
@@ -9,8 +9,11 @@ describe 'directors' do
 
       expect(response).to be_successful
       returned_director = JSON.parse(response.body, symbolize_names: true)[:data]
-
-
+      expect(returned_director[:id].to_i).to eq(director.id)
+      expect(returned_director).to have_key(:attributes)
+      expect(returned_director[:attributes]).to have_key(:email)
+      expect(returned_director[:attributes]).to have_key(:token)
+      expect(returned_director[:attributes]).to have_key(:google_id)
     end
   end
 end
