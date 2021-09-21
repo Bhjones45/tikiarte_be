2c4f9e4e67aa5b58138a984d40b29c52abdb4f8e
Amanda McGuire <aruwart001@gmail.com>
2021-09-21 15:19:33 -0600

    Add lorem picsum service to consume external api

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,3 +29,11 @@ RSpec.configure do |config|
     end
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+end
