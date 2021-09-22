69c0493b23420e8ba541e8217a8a367002c82d61
Amanda McGuire <aruwart001@gmail.com>
2021-09-22 12:40:10 -0600

    Test and route setup for get artist images

---
+++ spec/requests/api/v1/artist_images/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'artists images' do
+  before :each do
+    @artist = create(:artist, :with_images)
+  end
+
+  describe 'get images' do
+    it 'return all images for an artist' do
+
+      
+    end
+  end
+end
