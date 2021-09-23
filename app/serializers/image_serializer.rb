797c33e4cc6054d0aea92784a4b4a725437ca361
JoannaCoPo <jococopo@gmail.com>
2021-09-22 18:01:21 -0600

    Add upload url to serializers

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,4 +1,4 @@
 class ImageSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :status, :description, :tags, :title, :artist_id
+  attributes :status, :description, :tags, :title, :artist_id, :upload_url
 end
