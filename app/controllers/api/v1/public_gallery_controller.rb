1c2a517b4832b6ba7e976ea066f3c922bafd4ce4
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-23 14:17:26 -0600

    Update test and functionality to return public gallary images sorted by created at in ascending order

--- app/controllers/api/v1/public_gallery_controller.rb
+++ app/controllers/api/v1/public_gallery_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PublicGalleryController < ApplicationController
   def index
     public_images = Image.find_public_images
+    public_images.sorted
     render json: PublicGallerySerializer.new(public_images)
   end
 end
